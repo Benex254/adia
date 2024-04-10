75b9d1ea94ad51a8744db3bc2321593a2faab606
Benedict Xavier wanyonyi <81157281+BXW-855@users.noreply.github.com>
2024-04-10 17:52:00 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# The official repo for the Adia project 
-## Trully a snippet of daily wisdom
--- frontend/src/pages/Write.tsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "../components/NavBar.tsx"
-
-const WritePage = () => {
-  return (
-    <NavBar/>
-  )
-}
-
-export default WritePage
