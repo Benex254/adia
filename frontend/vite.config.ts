fe46e044007f4ee4e8b73d2872401113151ffc20
Benedict Xavier wanyonyi <81157281+BXW-855@users.noreply.github.com>
2024-04-10 17:50:17 +0000

    updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './build',
+    emptyOutDir: true, // Clean the output directory before each build
+  },
 })
